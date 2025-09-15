3d5af9442a4d2911247c170ecb564bf466c6ff25
Artemis C <98719680+frosty6742@users.noreply.github.com>
2025-09-15 10:48:21 -0600

    Ingest & condition Code
    Cleaned up ingest & condition for 15mps and 30mps. Assigned data to variables. Looped through ports.

--- Airfoil_Analysis_Main.m
+++ Airfoil_Analysis_Main.m
@@ -27,6 +27,8 @@ Segments = readtable('Port_Locations.xlsx','Sheet','Segments'); %Read in segment
 
 %% Search Data Folders, Pull File Names & Count Data Files
 % Get filenames for test data files
+
+% --- 15mps ---
 fileLoc = '15 mps Data Files/';
 list = dir([fileLoc, '*AoA*']); % This lists all files in fileLoc with 'WTData' in the file name
 numFiles = length(list);
@@ -38,6 +40,7 @@ end
 fileNames15 = string(fileNames15); % Convert to char array from cell array
 AoA_Count15 = length(AoA15); %Counts number of AoAs tested
 
+% --- 30mps ---
 fileLoc = '30 mps Data Files/';
 list = dir([fileLoc, '*AoA*']); % This lists all files in fileLoc with 'WTData' in the file name
 numFiles = length(list);
@@ -67,79 +70,70 @@ Data30 = zeros(numFiles,25); %Conditioned wind tunnel data file 30 m/s
     % Average Airfoil Port Local Velocity (done by student code)
     % Average Airfoil Port Local Static Pressure (done by student code)
 
-for j = 1:numFiles
-    RawData15 = readmatrix(fileNames15(j),'NumHeaderLines',1); % load the data
-    %Condition data
-    Data15(j,1) = AoA15(j); %Sets AoA (deg) as 1st column
-    Data15(j,2) = mean(RawData15(:,4)); %Sets mean of velocity measurements as 2nd column
-    Data15(j,3) = mean(RawData15(:,2)); %Sets mean of total pressure (atmosphere) as 3rd column
-    Data15(j,4) = mean(RawData15(:,1)); %Sets mean of temperature (atmosphere) as 4th column
-    Data15(j,5) = mean(RawData15(:,3)); %Sets mean of calculaed density (atmosphere) as 5th column
-    Data15(j,6) = mean(RawData15(:,5)); %Sets mean of test section dynamic pressure as 6th column
-    Data15(j,7) = Data15(j,3)-Data15(j,6); %Calculates test section static pressure by calculating Po - q
-    %Calculate airfoil port static pressures from measured differential
-    %pressues (P_port - P_static_test) & sets columns 8 - 24 as airfoil static port pressure values (calculated) 
-    % Data15(j,8) = ; %Airfoil scanivalve port 1
-    % Data15(j,9) = ; %Airfoil scanivalve port 2
-    % Data15(j,10) = ; %Airfoil scanivalve port 3
-    % Data15(j,11) = ; %Airfoil scanivalve port 4
-    % Data15(j,12) = ; %Airfoil scanivalve port 5
-    % Data15(j,13) = ; %Airfoil scanivalve port 6
-    % Data15(j,14) = ; %Airfoil scanivalve port 7
-    % Data15(j,15) = ; %Airfoil scanivalve port 8
-    % Data15(j,16) = ; %Airfoil scanivalve port 9
-    for k = 8:16 % Do each port in a for loop instead of line by line
-        Data15(j,k) = mean(RawData15(:,k+7))-Data15(j,7);
+%% --- Ingest & condition: 15mps ---
+for j = 1:numFiles15
+    RawData15 = readmatrix(fileNames15(j),'NumHeaderLines',1);
+
+    % Map columns
+    Data15(j,1) = AoA15(j);            % AoA
+    Data15(j,2) = mean(RawData15(:,4));% V_test
+    Data15(j,3) = mean(RawData15(:,2));% Patm 
+    Data15(j,4) = mean(RawData15(:,1));% Tatm
+    Data15(j,5) = mean(RawData15(:,3));% rho (calced density)
+    Data15(j,6) = mean(RawData15(:,5));% q_test (test section dynamic pressure)
+    Data15(j,7) = Data15(j,3) - Data15(j,6);  % P_static_test = Patm - q
+
+    % Ports 1 through 9: Raw differential is (P_port - P_static_test)
+    % Convert to absolute P_port = (P_port - P_static_test) + P_static_test.
+    for k = 8:16                      % k indexes output columns; 8 to port1, ..., 16 to port9
+        deltaP = mean(RawData15(:,k+7));   % raw col 15..23
+        Data15(j,k) = deltaP + Data15(j,7);
     end
-    Data15(j,17) = Data15(j,7); %TE - Assume Airfoil trailing edge pressure = test section static pressure
-    % Data15(j,18) = ; %Airfoil scanivalve port 10
-    % Data15(j,19) = ; %Airfoil scanivalve port 11
-    % Data15(j,20) = ; %Airfoil scanivalve port 12
-    % Data15(j,21) = ; %Airfoil scanivalve port 13
-    % Data15(j,22) = ; %Airfoil scanivalve port 14
-    % Data15(j,23) = ; %Airfoil scanivalve port 15
-    % Data15(j,24) = ; %Airfoil scanivalve port 16
-    for k = 18:24 % Same as above
-        Data15(j,k) = mean(RawData(:,k+6))-Data15(j,7);
+
+    % Trailing edge assumed static test section
+    Data15(j,17) = Data15(j,7);
+
+    % Ports 10..16
+    for k = 18:24                      % k indexes output columns; 18 to port10, ..., 24 to port16
+        deltaP = mean(RawData15(:,k+6));   % raw col 24..30
+        Data15(j,k) = deltaP + Data15(j,7);
     end
-    Data15(j,25) = Data15(j,8); %Repeats port 1 (leading edge)
+
+    Data15(j,25) = Data15(j,8); % Repeat port 1 
 
 end
-Data15 = sortrows(Data15,1); %Sorts data by increasing AoA
+Data15 = sortrows(Data15,1); % Sorts data by increasing Ao
 
-for j = 1:numFiles
-    RawData30 = readmatrix(fileNames30(j),'NumHeaderLines',1); % load the data
-    %Condition data
-    Data30(j,1) = AoA30(j); %Sets AoA (deg) as 1st column
-    Data30(j,2) = ; %Sets mean of velocity measurements as 2nd column
-    Data30(j,3) = ; %Sets mean of total pressure (atmosphere) as 3rd column
-    Data30(j,4) = ; %Sets mean of temperature (atmosphere) as 4th column
-    Data30(j,5) = ; %Sets mean of calculaed density (atmosphere) as 5th column
-    Data30(j,6) = ; %Sets mean of test section dynamic pressure as 6th column
-    Data30(j,7) = ; %Calculates test section static pressure by calculating Po - q
-    %Calculate airfoil port static pressures from measured differential
-    %pressues (P_port - P_static_test) & sets columns 8 - 24 as airfoil static port pressure values calculated 
-    Data30(j,8) = ; %Airfoil scanivalve port 1
-    Data30(j,9) = ; %Airfoil scanivalve port 2
-    Data30(j,10) = ; %Airfoil scanivalve port 3
-    Data30(j,11) = ; %Airfoil scanivalve port 4
-    Data30(j,12) = ; %Airfoil scanivalve port 5
-    Data30(j,13) = ; %Airfoil scanivalve port 6
-    Data30(j,14) = ; %Airfoil scanivalve port 7
-    Data30(j,15) = ; %Airfoil scanivalve port 8
-    Data30(j,16) = ; %Airfoil scanivalve port 9
-    Data30(j,17) = ; %TE  - Assume Airfoil trailing edge pressure = test section static pressure
-    Data30(j,18) = ; %Airfoil scanivalve port 10
-    Data30(j,19) = ; %Airfoil scanivalve port 11
-    Data30(j,20) = ; %Airfoil scanivalve port 12
-    Data30(j,21) = ; %Airfoil scanivalve port 13
-    Data30(j,22) = ; %Airfoil scanivalve port 14
-    Data30(j,23) = ; %Airfoil scanivalve port 15
-    Data30(j,24) = ; %Airfoil scanivalve port 16
-    Data30(j,25) = ; %Repeats port 1 (leading edge)
+%% --- Ingest & condition: 30mps ---
+for j = 1:numFiles30
+    RawData30 = readmatrix(fileNames30(j),'NumHeaderLines',1);
 
+    Data30(j,1) = AoA30(j);            % AoA
+    Data30(j,2) = mean(RawData30(:,4));% V_test
+    Data30(j,3) = mean(RawData30(:,2));% Patm
+    Data30(j,4) = mean(RawData30(:,1));% Tatm
+    Data30(j,5) = mean(RawData30(:,3));% rho
+    Data30(j,6) = mean(RawData30(:,5));% q_test
+    Data30(j,7) = Data30(j,3) - Data30(j,6);  % P_static_test
+
+    % Ports 1..9
+    for k = 8:16
+        deltaP = mean(RawData30(:,k+7));   % raw col 15..23
+        Data30(j,k) = deltaP + Data30(j,7);
+    end
+
+    % Trailing edge assumed static test section
+    Data30(j,17) = Data30(j,7);
+
+    % Ports 10..16
+    for k = 18:24
+        deltaP = mean(RawData30(:,k+6));   % raw col 24..30
+        Data30(j,k) = deltaP + Data30(j,7);
+    end
+
+    Data30(j,25) = Data30(j,8);  % Repeat port 1
 end
-Data30 = sortrows(Data30,1); %Sorts data by increasing AoA
+Data30 = sortrows(Data30,1);  % Sorts data by increasing AoA
 
 %% Determine Forces & Analyze Results
 % Pressure Distribution for each Velocity & AoA tested (done by student code)
